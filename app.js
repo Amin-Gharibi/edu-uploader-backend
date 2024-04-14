2fbc1fa1eec2a22e7f7e816e8cd12b6d3b4b96b4
Amin Gharibi <gharibi8364@gmail.com>
2024-04-14 16:32:38 +0330

    create app.js and server.js file

---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const express = require("express");
+const { setHeaders } = require("./middlewares/headers");
+const { errorHandler } = require("./middlewares/errors");
+
+//*routes import
+const authRoutes = require("routes/auth")
+const userRoutes = require("routes/user")
+
+const app = express();
+
+//* BodyParser
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "50mb" }));
+
+//* CORS Policy Definitions
+app.use(setHeaders);
+
+//* Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+
+
+//* Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+//* Error Controller
+app.use((req, res) => {
+	console.log("this path is not available:", req.path);
+	res.status(404).json({ message: "404 OOPS! PATH NOT FOUND" });
+});
+app.use(errorHandler);
+
+module.exports = app;
